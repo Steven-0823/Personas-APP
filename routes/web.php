743a625f8560ce6e4586ff03648c7e514dd31cee
Joan Steven Henao Bermeo <joan.henao@OMNICON.CC>
2024-03-28 13:31:47 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Comunas',[ComunaController::class,'index']) ->name('comuna.index');
